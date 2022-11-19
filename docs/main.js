34976f3595767a2b9532ede1bdb58800db603b2c
Marcos Freire <mfreire1996@gmail.com>
2022-11-18 23:27:42 -0500

    first commit

---
+++ docs/main.js
@@ -0,0 +1,8 @@
+function modalAnimation() {
+  let animation = document.getElementById("modal-animation");
+  if (animation.style.display === "none") {
+    animation.style.display = "block";
+  } else {
+    animation.style.display = "none";
+  }
+}
